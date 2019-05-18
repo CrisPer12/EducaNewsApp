7749686e3671e8472faa181835a5d89a3b5fba51
CriStegen12 <cristianopereiraramos9@gmail.com>
2019-05-18 16:36:05 -0300

    Adicionando matérias, conteudo

--- app/src/main/java/com/example/crist/educanews/educaActivity.java
+++ app/src/main/java/com/example/crist/educanews/educaActivity.java
@@ -1,5 +1,7 @@
 package com.example.crist.educanews;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +17,10 @@ public class educaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_educa);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    public void educaMat(View view){
+        Intent educaMatIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://educacao.uol.com.br/noticias/2019/04/02/falencia-de-grafica-inep-diz-que-cronograma-do-enem-2019-esta-mantido.htm"));
+        startActivity(educaMatIntent);
     }
 
 }
