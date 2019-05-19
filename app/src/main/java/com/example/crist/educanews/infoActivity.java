edb3f81b29b4f9063011472f578d1962c1e430a2
CriStegen12 <cristianopereiraramos9@gmail.com>
2019-05-18 22:57:14 -0300

    finish app

--- app/src/main/java/com/example/crist/educanews/infoActivity.java
+++ app/src/main/java/com/example/crist/educanews/infoActivity.java
@@ -1,6 +1,7 @@
 package com.example.crist.educanews;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -12,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class infoActivity extends AppCompatActivity {
 
@@ -21,6 +26,16 @@ public class infoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        VideoView videoView =findViewById(R.id.video_view);
+        String videoPath = "android.resource://com.example.crist.educanews/" + R.raw.hojetec;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
     }
     public void zenfone(View view){
         Intent zenfoneIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.tecmundo.com.br/dispositivos-moveis/141304-vaza-design-asus-zenfone-6-tela-notch-camera-giratoria.htm"));
@@ -30,4 +45,5 @@ public class infoActivity extends AppCompatActivity {
         Intent pedraIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.tecmundo.com.br/internet/141442-mulher-tem-casa-apedrejada-trocar-senha-wi-fi-veja-video.htm"));
         startActivity(pedraIntent);
     }
+
 }
